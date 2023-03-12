21086f0681c13954802c553734721b1e12f3ff51
arunabhnewar <newardevil@gmail.com>
2023-03-12 18:57:35 +0600

    redux and todos actions done

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import filterReducer from './filter/filterReducer';
+import todoReducer from './todos/todoReducer';
+
+// Create RootReducer function use combine reducer function
+const rootReducer = combineReducers({
+    todos: todoReducer,
+    filter: filterReducer,
+});
+
+
+export default rootReducer;
---
+++ src/redux/todos/todosActionType.js
@@ -0,0 +1,7 @@
+// Action Type identifiers
+export const TODO_ADDED = 'todos/todo_added';
+export const TODO_TOGGLED = 'todos/todo_toggled';
+export const TODO_COLOR_SELECTION = 'todos/todo_color_selection';
+export const TODO_DELETED = 'todos/todo_deleted';
+export const TODO_ALL_COMPLETED = 'todos/todo_all_completed';
+export const TODO_CLEAR_COMPLETED = 'todos/todo_clear_completed';
